7b55e838b3ca5baf1f8466544a76705c7808761b
Alifseen <alifseen@gmail.com>
2024-10-17 11:24:17 +0500

    Instructors version: Got the table header using pandas column, got total using panda[column name].sum(), and added the logo

--- Section 25.py
+++ Section 25.py
@@ -12,12 +12,12 @@ filepaths = glob.glob("Files/*.xlsx")
 ## 3. For loop to go over the filepath list to operate on each file.
 for path in filepaths:
 
-    ## 4. Initiate a PDF with portrait A4 with mm units and no auto page breaks. Do not initiate outside the loop.
+    ## 4. Initiate a PDF with portrait A4 with mm units and no auto page breaks inside the loop so a different pdf is created for each xlsx file
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.set_auto_page_break(False)
 
-    ## 12. Add a variable to store total from each row in the
-    total_amount = 0
+    ## 12. No need for this
+    # total_amount = 0
 
     ## 5. Load the excel file and sheet
     df = pd.read_excel(path, sheet_name="Sheet 1")
@@ -39,13 +39,15 @@ for path in filepaths:
     ## 9. Add line breaks for the table
     pdf.ln(7)
 
-    ## 10. Add table headers
+    ## 10. Add table headers using list comprehension and pandas columns method
+    columns = df.columns  ## New
+    columns = [item.replace("_", " ").title() for item in columns]
     pdf.set_font(family="Helvetica", style="B", size=10)
-    pdf.cell(w=30, h=7, txt="Product ID", ln=0, border=1)  ## no line break from first till last cell in row
-    pdf.cell(w=70, h=7, txt="Product Name", ln=0, border=1)
-    pdf.cell(w=30, h=7, txt="Quantity", ln=0, border=1)
-    pdf.cell(w=30, h=7, txt="Price per Unit", ln=0, border=1)
-    pdf.cell(w=30, h=7, txt="Total", ln=1, border=1) ## line break for next row
+    pdf.cell(w=30, h=7, txt=columns[0], ln=0, border=1)  ## no line break from first till last cell in row
+    pdf.cell(w=70, h=7, txt=columns[1], ln=0, border=1)
+    pdf.cell(w=30, h=7, txt=columns[2].split(" ")[0], ln=0, border=1)  ## Reduced "Amount Purchased" to "Amount" since it was taking too much space.
+    pdf.cell(w=30, h=7, txt=columns[3], ln=0, border=1)
+    pdf.cell(w=30, h=7, txt=columns[4], ln=1, border=1) ## line break for next row
 
     ## 11. Add content of the xlsx file into the table under the relevant table header
     for index, row in df.iterrows():
@@ -56,7 +58,6 @@ for path in filepaths:
         pdf.cell(w=30, h=7, txt=f"{row["amount_purchased"]}", ln=0, border=1)
         pdf.cell(w=30, h=7, txt=f"{row["price_per_unit"]}", ln=0, border=1)
         pdf.cell(w=30, h=7, txt=f"{row["total_price"]}", ln=1, border=1) ## line break for next row
-        total_amount = total_amount + row["total_price"]  ## Add the total of each product to a variable for reusability
 
     ## 13. Add the last row with total amount
     pdf.set_font(family="Times", style="", size=8)
@@ -64,13 +65,15 @@ for path in filepaths:
     pdf.cell(w=70, h=7, txt="", ln=0, border=1)
     pdf.cell(w=30, h=7, txt="", ln=0, border=1)
     pdf.cell(w=30, h=7, txt="", ln=0, border=1)
-    pdf.cell(w=30, h=7, txt=f"{total_amount}", ln=1, border=1)
+    pdf.cell(w=30, h=7, txt=str(df["total_price"].sum()), ln=1, border=1)  ## Add the total of each product using pandas
 
     ## 14. Add line breaks for the text
     pdf.ln(15)
     pdf.set_font(family="Times", style="B", size=10)
-    pdf.cell(w=0, h=7, txt=f"The total due amount is {total_amount} Euros", ln=1, border=0)
-    pdf.cell(w=0, h=7, txt=f"LVLUP", ln=1, border=0)
+    pdf.cell(w=0, h=7, txt=f"The total due amount is {df["total_price"].sum()} Euros", ln=1, border=0)
+    pdf.cell(w=15, h=7, txt=f"LVLUP")
+    pdf.image("Files/python_logo.jpg", w=7)  ## Added an image to the logo.
+    ## The image above was not loading before because the extension was jpeg, but FPDF only reads JPG, PNG or GIF, so I renamed it.
 
     ## 15. Output each file in the files folder.
     pdf.output(f"Files/{filename}.pdf")
